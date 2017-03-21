12e4ac3901b69686b719e76c58430405b68ec516
Andras Podobni <podobni.andras@gmail.com>
2017-03-22 00:42:39 +0100

    add NotEnough.java file, exercise 19

---
+++ Week-02/Day-2/workshop/src/NotEnough.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Asus on 2017-03-22.
+ */
+public class NotEnough {
+  public static void main(String[] args) {
+    int a = -8;
+
+    if (a == 0 | a < 0) {System.out.println("Not enough");}
+      else if (a == 1) {System.out.println("One");}
+        else if (a == 2) {System.out.println("Two");}
+          else if (a > 2) {System.out.println("A lot");}
+    }
+  }
+

56f1811665ed761b688b8e91d61542e686bcf7a4
Andras Podobni <podobni.andras@gmail.com>
2017-03-21 17:30:23 +0100

    add SacondsInADay.java file

---
+++ Week-02/Day-2/workshop/src/SecondsInADay.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Asus on 2017-03-21.
+ */
+public class SecondsInADay {
+  public static void main(String[] args) {
+    int currentHours = (3600 * 14);
+    int currentMinutes = (60 * 34);
+    int currentSeconds = 42;
+    int allcurrentSeconds = (currentSeconds + currentMinutes + currentHours);
+    int allSecondsADay = (3600 * 24);
+    System.out.println(allSecondsADay - allcurrentSeconds);
+    
+  }
+ }

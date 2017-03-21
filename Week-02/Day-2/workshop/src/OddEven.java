321fd8bdd4e9366b3995cac1d9a2f37d768f8832
Andras Podobni <podobni.andras@gmail.com>
2017-03-22 00:27:00 +0100

    add OddEven.java file, exercise 18

---
+++ Week-02/Day-2/workshop/src/OddEven.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Asus on 2017-03-22.
+ */
+public class OddEven {
+  public static void main(String[] args) {
+    int a = 11;
+    boolean even = (a % 2 == 0);
+    if (even) {
+      System.out.println("Even");
+    } else {
+        System.out.println("Odd");
+      }
+    }
+  }
+

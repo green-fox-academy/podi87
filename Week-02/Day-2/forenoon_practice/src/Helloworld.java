d4eb8298fc2aaf0e1a57a6c0edd165f71bec05b4
Andras Podobni <podobni.andras@gmail.com>
2017-03-21 13:24:46 +0100

    create workshop folder and move other files into the forenoon_practice folder

---
+++ Week-02/Day-2/forenoon_practice/src/Helloworld.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * Created by Asus on 2017-03-21.
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+
+        System.out.println("Ez a gyerek olyan\n\"okos\"...");
+        System.out.println(0.1 + 0.2);
+        System.out.println(true);
+
+
+        boolean isMarried = true;
+        String text = "majom";
+        byte b = 127;
+        short peaches = 1;
+        int apples = 123;
+        long pears = 1233;
+        double pi = 3.14;
+        System.out.println(peaches--);
+        apples = 23;
+        System.out.println(peaches);
+
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner .number();
+        System.out.println(number);
+
+    }
+}

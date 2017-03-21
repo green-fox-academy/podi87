f96f7f8374f2efe59b60d1c2a74307e9b8ce698e
Andras Podobni <podobni.andras@gmail.com>
2017-03-21 12:05:27 +0100

    Helloworld.src changes: byte,short,int,long,double

--- Week-02/Day-2/src/Helloworld.java
+++ Week-02/Day-2/src/Helloworld.java
@@ -1,8 +1,26 @@
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 /**
  * Created by Asus on 2017-03-21.
  */
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("ALMA");
+
+        System.out.println("Ez a gyerek olyan\n\"okos\"...");
+        System.out.println(0.1 + 0.2);
+        System.out.println(true);
+
+
+        boolean isMarried = true;
+        String text = "majom";
+        byte b = 127;
+        short peaches = 1;
+        int apples = 123;
+        long pears = 1233;
+        double pi = 3.14;
+        System.out.println(apples);
+        apples = 23;
+        System.out.println(apples);
+
     }
 }

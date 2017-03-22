e10fd32fb71927b3c55b8b6b09fd5e02d53407a0
Andras Podobni <podobni.andras@gmail.com>
2017-03-22 12:29:15 +0100

    add EvenNumbers.java file, exercise 24

---
+++ Week-02/Day-3/workshop/src/EvenNumbers.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Asus on 2017-03-22.
+ */
+public class EvenNumbers {
+  public static void main(String[] args) {
+
+    for (int even = 0; even < 500; even++) {
+      if (even % 2 == 0) {
+        System.out.println(even);
+      }
+    }
+  }
+}

dd61f96a8d4599b18a092ee09cc2f052fbf910c2
Andras Podobni <podobni.andras@gmail.com>
2017-03-22 16:00:43 +0100

    add AppendA.java file, exercise 3

---
+++ Week-02/Day-3/workshop/src/AppendA.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Asus on 2017-03-22.
+ */
+public class AppendA {
+  public static void main(String[] args) {
+    String am = "kuty";
+    System.out.println(appendA(am));
+  }
+  public static String appendA(String am) {
+    return am + "a";
+  }
+}

fd79cad2005358641364aa88f880d6ed60fc546f
Andras Podobni <podobni.andras@gmail.com>
2017-03-22 15:18:50 +0100

    add 'HOLY' Pyramid.java, exercise 28

--- Week-02/Day-3/workshop/src/Pyramid.java
+++ Week-02/Day-3/workshop/src/Pyramid.java
@@ -3,18 +3,20 @@ import java.util.Scanner;
 /**
  * Created by Asus on 2017-03-22.
  */
-public class Triangle {
+public class Pyramid {
   public static void main(String[] args) {
     Scanner star = new Scanner(System.in);
     System.out.println("Please type a number:");
     int ui = star.nextInt();
 
-    for (int i = 1; i < ui + 1; i++) {
-      for (int j = 0; j < i; j++) {
+    for (int i = 0; i < ui; i++) {
+      for (int j = 0; j < (ui-i) - 1; j++) {
+        System.out.print(" ");
+      }
+      for (int j = 0; j < (i*2) + 1; j++) {
         System.out.print("*");
       }
-      System.out.println(" ");
+      System.out.println();
     }
   }
 }
-

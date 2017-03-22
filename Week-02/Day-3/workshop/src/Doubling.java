80a4b98fa6083edbfc273f9a38d4e9a5aae28455
Andras Podobni <podobni.andras@gmail.com>
2017-03-22 15:41:31 +0100

    add Doubling.java file, exercise 1

---
+++ Week-02/Day-3/workshop/src/Doubling.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Asus on 2017-03-22.
+ */
+public class Doubling {
+  public static void main(String[] args) {
+    int ak = 123;
+    System.out.println(doubling(ak));
+  }
+  public static int doubling(int ak) {
+    return 2 * ak;
+  }
+}

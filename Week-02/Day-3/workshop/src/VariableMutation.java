66a7684782f4e115cc38d6fe2c88bef78f1409e9
Andras Podobni <podobni.andras@gmail.com>
2017-03-22 12:08:52 +0100

    VariableMutation.java is modified, exercise 22

--- Week-02/Day-3/workshop/src/VariableMutation.java
+++ Week-02/Day-3/workshop/src/VariableMutation.java
@@ -3,14 +3,36 @@
  */
 public class VariableMutation {
   public static void main(String[] args) {
-
     double a = 24;
     int out = 0;
-
     if (a % 2 == 0 ) {
-      out++;
-    }
+      out++;}
     System.out.println(out);
 
+    int b = 10;
+    String out2 = " ";
+    if (10 < b && b < 20) {System.out.println(out2 ="Sweet!");}
+      else if (b <= 10) {System.out.println(out2 ="More!");}
+      else if (b >= 20) {System.out.println(out2 ="Less!");}
+
+    int c = 123;
+    int credits = 100;
+    boolean isBonus = false;
+
+    if (credits >= 50 && isBonus == false) {System.out.println(c - 2);}
+    else if (credits < 50 && isBonus == false) {System.out.println(c - 1);}
+    else if (isBonus == true) {System.out.println(c);}
+
+    int d = 8;
+    int time = 120;
+    String out3 = " ";
+    if ((d % 4 == 0) && time <= 200) {System.out.println(out3 = "check");}
+    else if (time > 200) {System.out.println(out3 = "Time out");}
+    else if (d % 4 != 0) {System.out.println(out3 = "Run Forest Run!");}
+
+
+
   }
+
+
 }

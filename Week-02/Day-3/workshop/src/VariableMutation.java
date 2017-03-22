288586697ea00f69ecc22e22bf4b438097e331a1
Andras Podobni <podobni.andras@gmail.com>
2017-03-22 11:34:13 +0100

    create day-3 folder and add new VariableMutation.java file, exercise 22

---
+++ Week-02/Day-3/workshop/src/VariableMutation.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Asus on 2017-03-22.
+ */
+public class VariableMutation {
+  public static void main(String[] args) {
+
+    double a = 24;
+    int out = 0;
+
+    if (a % 2 == 0 ) {
+      out++;
+    }
+    System.out.println(out);
+
+  }
+}

769a401eaad3482c23051701d56e9d38871edbb2
Andras Podobni <podobni.andras@gmail.com>
2017-03-22 15:54:19 +0100

    add Greetings.java file, exercise 2

---
+++ Week-02/Day-3/workshop/src/Greetings.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Asus on 2017-03-22.
+ */
+public class Greetings {
+  public static void main(String[] args) {
+    String a1 = "Greenfox";
+    System.out.println(greet(a1));
+  }
+  public static String greet(String a1) {
+    return "Greetings dear, " + a1;
+  }
+}
+
+

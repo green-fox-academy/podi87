e3ed8ccb5445cd6e2bc4a47064c1bfa117d1dd8b
Andras Podobni <podobni.andras@gmail.com>
2017-03-23 18:36:12 +0100

    Matchmaking.java file modified with 'if'conditions

--- Week-02/Day-4/workshop/src/Matchmaking.java
+++ Week-02/Day-4/workshop/src/Matchmaking.java
@@ -9,16 +9,18 @@ public class Matchmaking {
     ArrayList<String> girls = new ArrayList<String>(Arrays.asList("Eve", "Ashley", "Bözsi", "Kat", "Jane"));
     ArrayList<String> boys = new ArrayList<String>(Arrays.asList("Joe", "Fred", "Béla", "Todd", "Neef", "Jeff"));
     ArrayList<String> order = new ArrayList<String>();
-
-    for (int i = 0; i < girls.size() + boys.size()-1; i +=2) {
-      order.add(i, girls.get(i/2));
-      order.add(i, boys.get(i/2));
+    int size = girls.size() > boys.size() ? girls.size() : boys.size();
+    for (int i = 0; i < boys.size(); i +=1) {
+      if (girls.size() > i) {
+        order.add(girls.get(i));
+      }
+      if (boys.size() > i) {
+        order.add(boys.get(i));
+      }
     }
     System.out.println(order);
   }
-
 }
-
 
 
 

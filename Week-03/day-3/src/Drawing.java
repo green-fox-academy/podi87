aa456f6db1c34be3e13f11a4c29da4941c10afdf
Andras Podobni <podobni.andras@gmail.com>
2017-03-29 11:14:19 +0200

    modifies the Drawing.java file with adding the drawing lines function

--- Week-03/day-3/src/Drawing.java
+++ Week-03/day-3/src/Drawing.java
@@ -1,33 +1,42 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
+
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class Drawing {
   public static void mainDraw(Graphics graphics) {
 //PART 1 - //  draw a red horizontal line to the canvas' middle.
-           //  draw a green vertical line to the canvas' middle.
+    //  draw a green vertical line to the canvas' middle.
     graphics.setColor(Color.red);
     graphics.drawLine(150, 0, 150, 300);
-    graphics.setColor(Color.green);
+    graphics.setColor(Color.CYAN);
     graphics.drawLine(0, 150, 300, 150);
 //PART 2 - // draw a box that has different colored lines on each edge.
     graphics.setColor(Color.black);
-    graphics.drawLine(0,0,0,300);
+    graphics.drawLine(0, 0, 0, 300);
     graphics.setColor(Color.yellow);
-    graphics.drawLine(0,0,300,0);
+    graphics.drawLine(0, 0, 300, 0);
     graphics.setColor(Color.orange);
-    graphics.drawLine(300,0,300,300);
+    graphics.drawLine(300, 0, 300, 300);
     graphics.setColor(Color.blue);
-    graphics.drawLine(300,300,0,300);
+    graphics.drawLine(300, 300, 0, 300);
 //PART 3 - // draw the canvas' diagonals in green.
     graphics.setColor(Color.GREEN);
-    graphics.drawLine(0,0,300,300);
+    graphics.drawLine(0, 0, 300, 300);
     graphics.setColor(Color.GREEN);
-    graphics.drawLine(300,0,0,300);
-
+    graphics.drawLine(300, 0, 0, 300);
+//PART 4 - // drawing lines function
+   for (int i = 0; i < 3; i++){
+     graphics.setColor(Color.BLACK);
+     drawLines(graphics);
+   }
   }
-
-
+  public static void drawLines(Graphics g) {
+    int x = (int) (Math.random() * 301);
+    int y = (int) (Math.random() * 301);
+    g.drawLine(x, y, 150, 150);
+  }
 
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");

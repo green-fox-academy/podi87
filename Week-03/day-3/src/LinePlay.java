a7e347f9fdb68416b39ae05eaf6bf4142699a42f
Andras Podobni <podobni.andras@gmail.com>
2017-03-29 17:32:27 +0200

    modifies the FunctionToCenter.java file and add LinePlay.java file, done

--- Week-03/day-3/src/LinePlay.java
+++ Week-03/day-3/src/LinePlay.java
@@ -4,37 +4,32 @@ import java.util.Random;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class FunctionToCenter {
+public class LinePlay {
   public static void mainDraw(Graphics graphics) {
-    int x1 = 0;
+    int x1 = 20;
     int y1 = 0;
-
-    for (int i = 0; i < 30; i++) {
-      if (x1 <= 300){
-        drawLines(graphics, x1, y1);
-        x1 += 20;
-      } else {
-        drawLines(graphics, x1, y1);
-        y1 += 20;
-      }
+    int x2 = 280;
+    int y2 = 20;
+    while (y2 <= 280 && x1 <= 260) {
+      graphics.setColor(new Color(177, 70, 244));
+      drawLines(graphics, x1, y1, x2, y2);
+      y2 += 20;
+      x1 += 20;
     }
-    for (int j = 0; j < 33; j++){
-      if (x1 >= 0){
-        drawLines(graphics, x1, y1);
-        x1 -= 20;
-      }else if (y1 >= 0) {
-        drawLines(graphics, x1, y1);
-        y1 -= 20;
-      }
+    int X1 = 20;
+    int Y1 = 20;
+    int X2 = 40;
+    int Y2 = 280;
+    while (Y1 <= 280 && X2 <= 280) {
+      graphics.setColor(Color.green);
+      drawLines(graphics, X1, Y1, X2, Y2);
+      Y1 += 20;
+      X2 += 20;
     }
   }
-
-  public static void drawLines(Graphics g, int x1, int y1){
-    int x2 = 150;
-    int y2 = 150;
+  public static void drawLines(Graphics g, int x1, int y1, int x2, int y2) {
     g.drawLine(x1, y1, x2, y2);
   }
-
   public static void main(String[] args) {
     JFrame jFrame = new JFrame("Drawing");
     jFrame.setSize(new Dimension(300, 300));
@@ -43,7 +38,8 @@ public class FunctionToCenter {
     jFrame.setLocationRelativeTo(null);
     jFrame.setVisible(true);
   }
-  static class ImagePanel extends JPanel{
+
+  static class ImagePanel extends JPanel {
     @Override
     protected void paintComponent(Graphics graphics) {
       super.paintComponent(graphics);
@@ -52,5 +48,5 @@ public class FunctionToCenter {
 
     }
   }
-
 }
+

63834059e3642e5bb35c905ffc343b1461884734
Andras Podobni <podobni.andras@gmail.com>
2017-03-29 17:50:26 +0200

    modifies LinePlay.java file

--- Week-03/day-3/src/LinePlay.java
+++ Week-03/day-3/src/LinePlay.java
@@ -6,27 +6,35 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 public class LinePlay {
   public static void mainDraw(Graphics graphics) {
-    int x1 = 20;
+    int x1 = 0;
     int y1 = 0;
-    int x2 = 280;
-    int y2 = 20;
-    while (y2 <= 280 && x1 <= 260) {
+    int x2 = 300;
+    int y2 = 0;
+    while (y2 < 300 && x1 < 300) {
       graphics.setColor(new Color(177, 70, 244));
       drawLines(graphics, x1, y1, x2, y2);
       y2 += 20;
       x1 += 20;
     }
-    int X1 = 20;
-    int Y1 = 20;
-    int X2 = 40;
-    int Y2 = 280;
-    while (Y1 <= 280 && X2 <= 280) {
+    int X1 = 0;
+    int Y1 = 0;
+    int X2 = 0;
+    int Y2 = 300;
+    while (Y1 < 300 && X2 < 300) {
       graphics.setColor(Color.green);
       drawLines(graphics, X1, Y1, X2, Y2);
       Y1 += 20;
       X2 += 20;
     }
+//    graphics.setColor(Color.GREEN);
+//    for(int i = 0; i < 300; i += 20) {
+//      graphics.setColor(Color.GREEN);
+//      graphics.drawLine(0, i, i, 300);
+//      graphics.setColor(new Color(142,0,237));
+//      graphics.drawLine(i, 0, 300, i);
+//    }
   }
+
   public static void drawLines(Graphics g, int x1, int y1, int x2, int y2) {
     g.drawLine(x1, y1, x2, y2);
   }

18c167e2a45c605bf829cc7ce0fbfaa722364d9b
Andras Podobni <podobni.andras@gmail.com>
2017-03-30 11:23:43 +0200

    corrects the SumDigit.java file, done

--- Week-03/day-4/src/SumDigits.java
+++ Week-03/day-4/src/SumDigits.java
@@ -1,15 +1,16 @@
-import java.util.Scanner;
 
 public class SumDigits {
   public static void main(String[] args) {
-    int number1 = 126;
+    int number1 = 2368;
     System.out.println(sumDig(number1));
   }
   public static int sumDig(int n) {
+    int temp = (n) - (n % 10);
+    int div = temp / 10;
     if (n < 10 || n == 0) {
       return n;
     } else {
-      return n % 10 + sumDig(n = n / 10);
+      return div;
     }
   }
 }

2f1900d71a6ce1639483788ce348a9f069c09466
Andras Podobni <podobni.andras@gmail.com>
2017-04-11 15:39:02 +0200

    add skeleton.png file, initial skeleton drawing is done, but the skeleton is still moving.

--- Week-05/Wanderer/src/Board.java
+++ Week-05/Wanderer/src/Board.java
@@ -7,8 +7,11 @@ import java.util.List;
 
 public class Board extends JComponent implements KeyListener {
 
-  int testBoxX;
-  int testBoxY;
+  int heroBoxX;
+  int heroBoxY;
+  int skeleBoxX;
+  int skeleBoxY;
+  int skeleNUm = 3;
   String heroPic = "hero-down.png";
   int[][] wallMatrix = new int[][]{
           {0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
@@ -23,11 +26,9 @@ public class Board extends JComponent implements KeyListener {
           {0, 0, 0, 1, 0, 1, 1, 0, 1, 0},};
 
 
-
   public Board() {
-    testBoxX = 0;
-    testBoxY = 0;
-
+    heroBoxX = 0;
+    heroBoxY = 0;
 
     setPreferredSize(new Dimension(720, 720));
     setVisible(true);
@@ -36,23 +37,30 @@ public class Board extends JComponent implements KeyListener {
   @Override
   public void paint(Graphics graphics) {
     super.paint(graphics);
-    PositionedImage hero = new PositionedImage(heroPic, testBoxX, testBoxY);
+    List<Integer> availableX = new ArrayList<>();
+    List<Integer> availableY = new ArrayList<>();
+    PositionedImage hero = new PositionedImage(heroPic, heroBoxX, heroBoxY);
+    PositionedImage skeleton = new PositionedImage("skeleton.png", skeleBoxX * 72, skeleBoxY * 72);
 
     for (int row = 0; row < wallMatrix.length; row++) {
       for (int column = 0; column < wallMatrix[row].length; column++) {
         if (wallMatrix[row][column] == 0) {
           PositionedImage floor = new PositionedImage("floor.png", column * 72, row * 72);
           floor.draw(graphics);
+          availableX.add(floor.posX/72);
+          availableY.add(floor.posY/72);
         } else {
           PositionedImage wall = new PositionedImage("wall.png", column * 72, row * 72);
           wall.draw(graphics);
-
         }
       }
       hero.draw(graphics);
+      int temp = (int)(Math.random() * availableX.size());
+      skeleBoxX = availableX.get(temp);
+      skeleBoxY = availableY.get(temp);
+      skeleton.draw(graphics);
     }
   }
-
 
   // To be a KeyListener the class needs to have these 3 methods in it
   @Override
@@ -68,20 +76,19 @@ public class Board extends JComponent implements KeyListener {
   // But actually we can use just this one for our goals here
   @Override
   public void keyReleased(KeyEvent e) {
-        if ((e.getKeyCode() == KeyEvent.VK_UP) && (testBoxY > 0) && (wallMatrix[testBoxY/72 - 1][testBoxX/72] == 0)) {
-          testBoxY -= 72;
+        if ((e.getKeyCode() == KeyEvent.VK_UP) && (heroBoxY > 0) && (wallMatrix[heroBoxY /72 - 1][heroBoxX /72] == 0)) {
+          heroBoxY -= 72;
           heroPic = "hero-up.png";
-        } else if ((e.getKeyCode() == KeyEvent.VK_DOWN) && (testBoxY < 648) && ((wallMatrix[testBoxY/72 + 1][testBoxX/72] == 0))) {
-          testBoxY += 72;
+        } else if ((e.getKeyCode() == KeyEvent.VK_DOWN) && (heroBoxY < 648) && ((wallMatrix[heroBoxY /72 + 1][heroBoxX /72] == 0))) {
+          heroBoxY += 72;
           heroPic = "hero-down.png";
-        } else if ((e.getKeyCode() == KeyEvent.VK_LEFT) && (testBoxX > 0) && ((wallMatrix[testBoxY/72][testBoxX/72 - 1] == 0))) {
-          testBoxX -= 72;
+        } else if ((e.getKeyCode() == KeyEvent.VK_LEFT) && (heroBoxX > 0) && ((wallMatrix[heroBoxY /72][heroBoxX /72 - 1] == 0))) {
+          heroBoxX -= 72;
           heroPic = "hero-left.png";
-        } else if ((e.getKeyCode() == KeyEvent.VK_RIGHT) && (testBoxX < 648) && ((wallMatrix[testBoxY/72][testBoxX/72 + 1] == 0))) {
-          testBoxX += 72;
+        } else if ((e.getKeyCode() == KeyEvent.VK_RIGHT) && (heroBoxX < 648) && ((wallMatrix[heroBoxY /72][heroBoxX /72 + 1] == 0))) {
+          heroBoxX += 72;
           heroPic = "hero-right.png";
         }
-        // and redraw to have a new picture with the new coordinates
         repaint();
       }
 }

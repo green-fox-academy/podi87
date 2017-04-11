a8f73797554a968baa794b01f8ccdf2fd0988693
Andras Podobni <podobni.andras@gmail.com>
2017-04-11 14:17:54 +0200

    add hero-left -right -up.png files, hero direction, wall boundaries are done, map bounaries was solved earlier, but forgot to commit.

--- Week-05/Wanderer/src/Board.java
+++ Week-05/Wanderer/src/Board.java
@@ -2,15 +2,32 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.*;
+import java.util.List;
 
 public class Board extends JComponent implements KeyListener {
 
   int testBoxX;
   int testBoxY;
+  String heroPic = "hero-down.png";
+  int[][] wallMatrix = new int[][]{
+          {0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+          {0, 0, 0, 1, 0, 1, 0, 1, 1, 0},
+          {0, 1, 1, 1, 0, 1, 0, 1, 1, 0},
+          {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+          {1, 1, 1, 1, 0, 1, 1, 1, 1, 0},
+          {0, 1, 0, 1, 0, 0, 0, 0, 1, 0},
+          {0, 1, 0, 1, 0, 1, 1, 0, 0, 0},
+          {0, 0, 0, 0, 0, 1, 1, 0, 1, 0},
+          {0, 1, 1, 1, 0, 0, 0, 0, 1, 0},
+          {0, 0, 0, 1, 0, 1, 1, 0, 1, 0},};
+
+
 
   public Board() {
     testBoxX = 0;
     testBoxY = 0;
+
 
     setPreferredSize(new Dimension(720, 720));
     setVisible(true);
@@ -19,18 +36,7 @@ public class Board extends JComponent implements KeyListener {
   @Override
   public void paint(Graphics graphics) {
     super.paint(graphics);
-    PositionedImage hero = new PositionedImage("hero-down.png", testBoxX, testBoxY);
-    int[][] wallMatrix = new int[][]{
-            {0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
-            {0, 0, 0, 1, 0, 1, 0, 1, 1, 0},
-            {0, 1, 1, 1, 0, 1, 0, 1, 1, 0},
-            {0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-            {1, 1, 1, 1, 0, 1, 1, 1, 1, 0},
-            {0, 1, 0, 1, 0, 0, 0, 0, 1, 0},
-            {0, 1, 0, 1, 0, 1, 1, 0, 1, 0},
-            {0, 0, 0, 0, 0, 1, 1, 0, 1, 0},
-            {0, 1, 1, 1, 0, 0, 0, 0, 1, 0},
-            {0, 0, 0, 1, 0, 1, 1, 0, 1, 0},};
+    PositionedImage hero = new PositionedImage(heroPic, testBoxX, testBoxY);
 
     for (int row = 0; row < wallMatrix.length; row++) {
       for (int column = 0; column < wallMatrix[row].length; column++) {
@@ -40,10 +46,11 @@ public class Board extends JComponent implements KeyListener {
         } else {
           PositionedImage wall = new PositionedImage("wall.png", column * 72, row * 72);
           wall.draw(graphics);
+
         }
       }
+      hero.draw(graphics);
     }
-    hero.draw(graphics);
   }
 
 
@@ -61,18 +68,20 @@ public class Board extends JComponent implements KeyListener {
   // But actually we can use just this one for our goals here
   @Override
   public void keyReleased(KeyEvent e) {
-
-    // When the up or down keys hit, we change the position of our box
-    if ((e.getKeyCode() == KeyEvent.VK_UP) && testBoxY > 0 ) {
-      testBoxY -= 72;
-    } else if((e.getKeyCode() == KeyEvent.VK_DOWN) && testBoxY < 648) {
-      testBoxY += 72;
-    } else if((e.getKeyCode() == KeyEvent.VK_LEFT) && testBoxX > 0) {
-      testBoxX -= 72;
-    }else if((e.getKeyCode() == KeyEvent.VK_RIGHT) && testBoxX < 648) {
-      testBoxX += 72;
-    }
-    // and redraw to have a new picture with the new coordinates
-    repaint();
-  }
+        if ((e.getKeyCode() == KeyEvent.VK_UP) && (testBoxY > 0) && (wallMatrix[testBoxY/72 - 1][testBoxX/72] == 0)) {
+          testBoxY -= 72;
+          heroPic = "hero-up.png";
+        } else if ((e.getKeyCode() == KeyEvent.VK_DOWN) && (testBoxY < 648) && ((wallMatrix[testBoxY/72 + 1][testBoxX/72] == 0))) {
+          testBoxY += 72;
+          heroPic = "hero-down.png";
+        } else if ((e.getKeyCode() == KeyEvent.VK_LEFT) && (testBoxX > 0) && ((wallMatrix[testBoxY/72][testBoxX/72 - 1] == 0))) {
+          testBoxX -= 72;
+          heroPic = "hero-left.png";
+        } else if ((e.getKeyCode() == KeyEvent.VK_RIGHT) && (testBoxX < 648) && ((wallMatrix[testBoxY/72][testBoxX/72 + 1] == 0))) {
+          testBoxX += 72;
+          heroPic = "hero-right.png";
+        }
+        // and redraw to have a new picture with the new coordinates
+        repaint();
+      }
 }

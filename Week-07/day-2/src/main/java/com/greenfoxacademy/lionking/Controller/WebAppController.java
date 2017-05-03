367e9a380d279d6620fdc3b7b5fc7f466be5ec2d
Andras Podobni <podobni.andras@gmail.com>
2017-05-03 14:57:20 +0200

    create Exercise3.html and modifies WebAppController.java file, exercise3 is done.

--- Week-07/day-2/src/main/java/com/greenfoxacademy/lionking/Controller/WebAppController.java
+++ Week-07/day-2/src/main/java/com/greenfoxacademy/lionking/Controller/WebAppController.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebAppController {
 
-  @RequestMapping("/Exercise2")
+  @RequestMapping("/Exercise3")
   public String addBankAccount(Model model) {
     BankAccount simba = new BankAccount("Simba", 2000, "lion");
     model.addAttribute("simba", simba);
-    return "Exercise2";
+    return "Exercise3";
   }
 }

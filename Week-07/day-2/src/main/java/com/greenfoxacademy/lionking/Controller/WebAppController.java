865e449ae8afa91ec45a3dc953ea1ee00d5dfb06
Andras Podobni <podobni.andras@gmail.com>
2017-05-03 14:48:16 +0200

    create Exercise2.html and modifies the WebAppController.java, exercise2 is done.

--- Week-07/day-2/src/main/java/com/greenfoxacademy/lionking/Controller/WebAppController.java
+++ Week-07/day-2/src/main/java/com/greenfoxacademy/lionking/Controller/WebAppController.java
@@ -5,14 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
 @Controller
 public class WebAppController {
 
-  @RequestMapping("/Exercise1")
+  @RequestMapping("/Exercise2")
   public String addBankAccount(Model model) {
     BankAccount simba = new BankAccount("Simba", 2000, "lion");
     model.addAttribute("simba", simba);
-    return "Exercise1";
+    return "Exercise2";
   }
 }
--- Week-07/day-2/src/main/java/com/greenfoxacademy/lionking/Model/BankAccount.java
+++ Week-07/day-2/src/main/java/com/greenfoxacademy/lionking/Model/BankAccount.java
@@ -1,11 +1,15 @@
 package com.greenfoxacademy.lionking.Model;
 
+import jdk.nashorn.internal.objects.annotations.Constructor;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.text.DecimalFormat;
 
 @Getter
 @AllArgsConstructor
 public class BankAccount {
+
   private String name;
   private int balance;
   private String animalType;
